6a462d7678cae74c1c9bc21d40bd0dfb23230aaa
darwin-mont <darwinm95@live.com>
2025-08-10 22:14:13 -0400

    project 10 - WTWr- 1v

---
+++ WTWr/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__author">Developed by Darwin Montaleza</p>
+      <p className="footer__date">2025</p>
+    </footer>
+  );
+}
+export default Footer;
